812fad9f831f4d02c8fd6981b1a6f0a3d2a09d59
YudiR <yudrose13@gmail.com>
2019-05-07 12:26:49 -0400

    added validation for location in items. Made migration for joining tables between user and items for item watching

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,9 @@
 class Item < ApplicationRecord
     belongs_to :user
+    has_and_belongs_to_many :users
 
     validates :title, presence: true
     validates :price, presence: true
     validates :description, presence: true
+    validates :location, presence: true 
 end
