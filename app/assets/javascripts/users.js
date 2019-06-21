b6b23771c96fc3e983fa899d14ce222c7f2093d7
YudiR <yudrose13@gmail.com>
2019-06-21 16:47:28 -0400

    figured out one functioning validation on item#new for title. code is in users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,3 +1,35 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+// document.addEventListener("DOMContentLoaded", function() {
+  var logInForm = document.querySelector("#log_in")
+    var logInEmail = document.querySelector("#log_in #email");
+    // var log = document.forms["log_in"]["email"].value
+
+    var newItem = document.querySelector("#new_item")
+    console.log(newItem)
+  console.log(logInEmail)
+  var logIn;
+
+//   var form = () => === "" ? alert("Email must be filled out") : (null) 
+
+function validateForm() {
+    var newItemTitle = document.querySelector("#new_item #item_title")
+
+    // var x = document.forms["log_in"]["email"].value;
+    if ( newItemTitle.value == "") {
+      alert("Name must be filled out");
+      return false;
+      
+    }
+  
+}
+
+
+
+//   logInForm.addEventListener("submit", function(e) {
+    
+    //  () => logInEmail.value === "" ? alert("Email must be filled out") : (null) 
+
+
+
+
+//   });
+// });
