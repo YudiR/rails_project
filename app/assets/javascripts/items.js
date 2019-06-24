bb3d4fa7a7e214c720c90d55bfcb82a03ddcf7b8
YudiR <yudrose13@gmail.com>
2019-06-24 16:27:48 -0400

    all users form are setup with validations

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,5 +1,11 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+// alert for title#item
+function validateForm() {
+    var newItemTitle = document.querySelector("#new_item #item_title")
 
-
+    if ( newItemTitle.value == "") {
+      alert("Name must be filled out");
+      return false;
+      
+    }
+  
+}
--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,35 +1,77 @@
-// document.addEventListener("DOMContentLoaded", function() {
-  var logInForm = document.querySelector("#log_in")
-    var logInEmail = document.querySelector("#log_in #email");
-    // var log = document.forms["log_in"]["email"].value
+//                                  ALERTS FOR LOGIN
+function logIn() {
+  //   email:
+  var logInEmail = document.querySelector("#log_in #email");
+  var emailAlertDiv = document.querySelector(".alert_email");
+  //  password:
+  var logInPass = document.querySelector(".log_in_password #password");
+  var passwordAlertDiv = document.querySelector(
+    ".log_in_password .alert_password"
+  );
+  // validation:
+  if (logInPass.value === "" || logInEmail.value === "") {
+    logInPass.value === ""
+      ? ((passwordAlertDiv.innerHTML = " Please enter your password"),
+        (passwordAlertDiv.style.color = "red"))
+      : null;
 
-    var newItem = document.querySelector("#new_item")
-    console.log(newItem)
-  console.log(logInEmail)
-  var logIn;
+    logInEmail.value === ""
+      ? ((emailAlertDiv.innerHTML = "Please enter your email"),
+        (emailAlertDiv.style.color = "red"))
+      : null;
 
-//   var form = () => === "" ? alert("Email must be filled out") : (null) 
+    logInEmail.value !== "" ? (emailAlertDiv.innerHTML = null) : null;
+    logInPass.value !== "" ? (passwordAlertDiv.innerHTML = null) : null;
 
-function validateForm() {
-    var newItemTitle = document.querySelector("#new_item #item_title")
-
-    // var x = document.forms["log_in"]["email"].value;
-    if ( newItemTitle.value == "") {
-      alert("Name must be filled out");
-      return false;
-      
-    }
-  
+    return false;
+  }   
 }
 
+//                            ALERTS FOR SIGN UP
 
+var validateUser = () => {
+  // name
+  var nameNew = document.querySelector("#user_name");
+  var nameDiv = document.querySelector(".nameSign");
+  // email
+  var emailNew = document.querySelector("#user_email");
+  var emailDiv = document.querySelector(".emailSign");
+  // password
+  var passwordNew = document.querySelector("#user_password");
+  var passwordDiv = document.querySelector(".passwordSign");
+  // password confirmation 
+  var passwordConfirmationNew = document.querySelector("#user_password_confirmation");
+  var passwordConfirmationDiv = document.querySelector(".passwordConfirmationSign");
 
-//   logInForm.addEventListener("submit", function(e) {
+  if (nameNew.value === "" || emailDiv.value === "" || passwordNew.value === "" || passwordConfirmationNew.value === "") {
+    nameNew.value === ""
+      ? ((nameDiv.innerHTML = " Please enter your Name"),
+        (nameDiv.style.color = "red"))
+      : null;
+
+    emailNew.value === ""
+      ? ((emailDiv.innerHTML = "Please enter your Email"),
+        (emailDiv.style.color = "red"))
+      : null;
     
-    //  () => logInEmail.value === "" ? alert("Email must be filled out") : (null) 
+      passwordNew.value === ""
+      ? ((passwordDiv.innerHTML = "Please enter your Password"),
+        (passwordDiv.style.color = "red"))
+      : null;
+    
+      passwordConfirmationNew.value === ""
+      ? ((passwordConfirmationDiv.innerHTML = "Please enter your Password Confirmation"),
+        (passwordConfirmationDiv.style.color = "red"))
+      : null;
+      
+      
+      nameNew.value !== "" ? (nameDiv.innerHTML = null) : null;
+      emailNew.value !== "" ? (emailDiv.innerHTML = null) : null;
+      passwordNew.value !== "" ? (passwordDiv.innerHTML = null) : null;
+      passwordConfirmationNew.value !== "" ? (passwordConfirmationDiv.innerHTML = null) : null;
 
+      return false;
 
+  }   
 
-
-//   });
-// });
+}
