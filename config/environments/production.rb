cad196ef09c94c4f4badccc3568c2f1f0010ec8c
YudiR <yudrose13@gmail.com>
2019-08-21 13:22:25 -0400

    removed  asset pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.assets.enabled = true
+
   rails precompile:assets
   # Code is not reloaded between requests.
   config.cache_classes = true
