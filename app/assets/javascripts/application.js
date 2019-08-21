0d5e793fc0d677e61fd17ebdd0a975237b983c0b
YudiR <yudrose13@gmail.com>
2019-08-21 14:25:58 -0400

    adding mini racer gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-// below is added
-//= require js-datepicker/datepicker.min
 //= require rails-ujs
 //= require activestorage
 //= require_tree .
